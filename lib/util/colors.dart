44399cee2848e4dc330e3e44d6d5c604a7a6e90d
fedor-bystrov <augustbystrov@gmail.com>
2020-05-12 20:05:25 +0300

    update accentColor

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class CommonColors {
   static final Color primaryColor = Colors.grey[300];
-  static final Color accentColor = Colors.indigo[500];
+  static final Color accentColor = Colors.indigo[700];
   static final Color blackInactive = Colors.black.withOpacity(0.54);
 }
