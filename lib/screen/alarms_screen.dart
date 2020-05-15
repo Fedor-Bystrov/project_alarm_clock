2a9cfb18cdd738096da604cf834b6e5eee514257
fedor-bystrov <augustbystrov@gmail.com>
2020-05-15 23:38:50 +0300

    rename components package to widgets

--- lib/screen/alarms_screen.dart
+++ lib/screen/alarms_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:projectbudy/component/alarm_tile.dart';
-import 'package:projectbudy/component/common_bottom_nav_bar.dart';
+import 'package:projectbudy/widget//alarm_tile.dart';
+import 'package:projectbudy/widget//common_bottom_nav_bar.dart';
 import 'package:projectbudy/util/colors.dart';
 import 'package:projectbudy/screen/add_alarm_screen.dart';
 import 'package:projectbudy/util/constants.dart' as constants;
