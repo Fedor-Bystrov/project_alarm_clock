54f92403ef615f56ec1e6911ec50164e5cf4345a
fedor-bystrov <augustbystrov@gmail.com>
2020-05-17 14:59:31 +0300

    clean up alarms_state test data

--- lib/state/alarms_state.dart
+++ lib/state/alarms_state.dart
@@ -7,8 +7,8 @@ class AlarmsState extends ChangeNotifier {
   List<Alarm> _alarms = [
     Alarm(DateTime.parse("2012-02-27 08:29:00"), "Mon Wed Fri", "Don’t Sleep", true),
     Alarm(DateTime.parse("2012-02-27 10:00:00"), "Every Day", "Alarm", false),
-    Alarm(DateTime.parse("2012-02-27 13:00:00"), "", "Alarm", true),
-    Alarm(DateTime.parse("2012-02-27 13:00:00"), "Every Day", "Alarm", false),
+    Alarm(DateTime.parse("2012-02-27 13:00:00"), null, "Alarm", true),
+    Alarm(DateTime.parse("2012-02-27 18:20:00"), "Every Day", "Alarm", false),
   ];
 
   UnmodifiableListView<Alarm> get alarms {
