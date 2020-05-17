1920ccde1fa26a0b401094990a7d2ee2476cac1d
fedor-bystrov <augustbystrov@gmail.com>
2020-05-17 18:06:33 +0300

    add alarm manager initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
+
 import 'package:projectbudy/application.dart';
 
+/// The name associated with the UI isolate's [SendPort].
+const String isolateName = 'mainProjectBudiUIisolate';
+
+/// A port used to communicate from a background isolate to the UI isolate.
+final ReceivePort receivePort = ReceivePort();
+
 main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Register the UI isolate's SendPort to allow for communication from the
+  // background isolate.
+  bool alarmManagerInitialized = await AndroidAlarmManager.initialize();
+  if (!alarmManagerInitialized) {
+    throw 'Alarm Manager Not Initialized!'; // TODO error handling, logging
+  }
+  IsolateNameServer.registerPortWithName(receivePort.sendPort, isolateName);
   runApp(Application());
 }
