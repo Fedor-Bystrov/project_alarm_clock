509c0ba6bc50aad0ef241443a630597871e48841
fedor-bystrov <augustbystrov@gmail.com>
2020-05-28 22:37:27 +0300

    improve AlarmsState#switchAlarm

--- lib/state/alarms.dart
+++ lib/state/alarms.dart
@@ -59,9 +59,9 @@ class AlarmsState extends ChangeNotifier {
     await persistAndNotify();
   }
 
-  void switchAlarm(Alarm alarm, bool value) { // TODO disable alarm job
+  Future<void> switchAlarm(Alarm alarm, bool value) async { // TODO disable alarm job
     alarm.enabled = value;
-    persistAndNotify();
+    await persistAndNotify();
   }
 
   Future<void> persistAndNotify() async {
