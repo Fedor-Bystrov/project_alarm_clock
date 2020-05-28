5c74f41aacbe37745826f40fdf03ec0b34da7aaf
fedor-bystrov <augustbystrov@gmail.com>
2020-05-28 22:24:09 +0300

    store alarms in SplayTreeSet

--- lib/state/alarms.dart
+++ lib/state/alarms.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:convert';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,18 +14,21 @@ class AlarmsState extends ChangeNotifier {
 
   final SharedPreferences sharedPreferences;
   AlarmManager _alarmManager;
-  List<Alarm> _alarms;
+  SplayTreeSet<Alarm> _alarms;
 
   AlarmsState(this.sharedPreferences) {
     _alarmManager = AlarmManager.getInstance();
-    _alarms = (sharedPreferences.getStringList(alarmsKey) ?? <String>[])
-        .map((a) => json.decode(a))
-        .map((a) => Alarm.fromJson(a))
-        .toList();
+
+    final Iterable<Alarm> alarmsIterable =
+        (sharedPreferences.getStringList(alarmsKey) ?? <String>[])
+            .map((a) => json.decode(a))
+            .map((a) => Alarm.fromJson(a));
+
+    _alarms = SplayTreeSet.of(alarmsIterable, (a, b) => a.time.compareTo(b.time));
   }
 
-  UnmodifiableListView<Alarm> get alarms {
-    return UnmodifiableListView(_alarms);
+  UnmodifiableSetView<Alarm> get alarms {
+    return UnmodifiableSetView(_alarms);
   }
 
   int get alarmsCount {
@@ -49,14 +53,14 @@ class AlarmsState extends ChangeNotifier {
     await _alarmManager.addAlarm(alarm);
   }
 
-  Future<void> deleteAlarm(int alarmIndex) async {
-    await _alarmManager.cancel(_alarms[alarmIndex]);
-    _alarms.removeAt(alarmIndex);
+  Future<void> deleteAlarm(Alarm alarm) async {
+    await _alarmManager.cancel(alarm);
+    _alarms.remove(alarm);
     await persistAndNotify();
   }
 
-  void switchAlarm(int alarmIndex, bool value) {
-    _alarms[alarmIndex].enabled = value;
+  void switchAlarm(Alarm alarm, bool value) { // TODO disable job
+    alarm.enabled = value;
     persistAndNotify();
   }
 
