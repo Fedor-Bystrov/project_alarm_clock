40df4b29584cb3460b0e8d718d48b5f9d59380e2
fedor-bystrov <augustbystrov@gmail.com>
2020-05-28 22:41:23 +0300

    fix edit_alarm

--- lib/screen/edit_alarm.dart
+++ lib/screen/edit_alarm.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:projectbudy/model/alarm.dart';
 import 'package:projectbudy/state/alarms.dart';
 import 'package:projectbudy/widget/appbar/edit_alarm.dart';
 import 'package:projectbudy/widget/body/edit_alarm.dart';
@@ -13,20 +14,19 @@ class EditAlarmScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final state = Provider.of<AlarmsState>(context);
-    final Map<String, int> arguments = ModalRoute.of(context).settings.arguments;
-    final alarm = state.alarms[arguments['alarmIndex']];
+    final Map<String, Alarm> arguments = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: EditAlarmAppBar(
           context: context,
           title: const Text("Edit Alarm"), // TODO translation
           onPressed: () async {
-            await state.updateAlarm(alarm);
+            await state.updateAlarm(arguments['alarm']);
             Navigator.pop(context);
           }),
       bottomNavigationBar: BottomNavBar(),
       body: EditAlarmBody(
         context: context,
-        alarm: alarm,
+        alarm: arguments['alarm'],
       ),
     );
   }
