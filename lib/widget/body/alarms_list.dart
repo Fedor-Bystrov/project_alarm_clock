942c7585cb7ad9e4b40ca6231f38627edc23bb08
fedor-bystrov <augustbystrov@gmail.com>
2020-06-12 22:32:04 +0300

    add EditAlarmTile

--- lib/widget/body/alarms_list.dart
+++ lib/widget/body/alarms_list.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:projectbudy/model/alarm.dart';
-import 'package:projectbudy/widget/body/component/default_alarm_tile.dart';
 import 'package:projectbudy/state/alarms.dart';
+import 'package:projectbudy/state/alarms_list.dart';
+import 'package:projectbudy/widget/body/component/default_alarm_tile.dart';
+import 'package:projectbudy/widget/body/component/edit_alarm_tile.dart';
 
 class AlarmsList extends StatelessWidget {
   @override
@@ -13,14 +15,20 @@ class AlarmsList extends StatelessWidget {
         return ListView(
           children: ListTile.divideTiles(
             context: context,
-            tiles: getAlarmTiles(state),
+            tiles: getAlarmTiles(state, context),
           ).toList(),
         );
       },
     );
   }
 
-  static Iterable<Widget> getAlarmTiles(AlarmsState state) {
-    return state.alarms.map((alarm) => Focus(key: ValueKey<Alarm>(alarm), child: DefaultAlarmTile(alarm)));
+  static Iterable<Widget> getAlarmTiles(AlarmsState state, BuildContext context) {
+    var listState = Provider.of<AlarmsListState>(context);
+    return state.alarms.map(
+      (alarm) => Focus(
+        key: ValueKey<Alarm>(alarm),
+        child: listState.isDefault() ? DefaultAlarmTile(alarm) : EditAlarmTile(alarm),
+      ),
+    );
   }
 }
---
+++ lib/widget/body/component/edit_alarm_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:projectbudy/model/alarm.dart';
+
+class EditAlarmTile extends StatelessWidget {
+  final Alarm _alarm;
+
+  EditAlarmTile(this._alarm);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("nu");
+  }
+}
